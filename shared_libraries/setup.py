df9d0a77d63ab4a798c9f630171b2dd8a667fdf2
Zachary Roehrich <zroehrich@contractor.usgs.gov>
2023-11-29 12:55:42 -0600

    jobs 0

--- shared_libraries/setup.py
+++ shared_libraries/setup.py
@@ -7,8 +7,8 @@ import sys
 import orca_shared.__init__ as library_list
 
 # Borrowed from pip at https://github.com/pypa/pip/blob/62c27dee45625e1b63d1e023b0656310f276e050/setup.py#L11-L15
-here = os.path.abspath(os.path.dirname(__file__))
-
+#here = os.path.abspath(os.path.dirname(__file__))
+here = os.chdir(os.path.dirname(__file__))
 
 def read(*parts):
     # intentionally *not* adding an encoding option to open, See:
